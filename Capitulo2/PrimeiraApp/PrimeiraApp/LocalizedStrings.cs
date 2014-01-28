8d9af34ca1ba4b81c488faf14ac0321d1a20ed8f
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2014-01-28 00:59:20 -0200

    Commit Diário

---
+++ Capitulo2/PrimeiraApp/PrimeiraApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PrimeiraApp.Resources;
+
+namespace PrimeiraApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
