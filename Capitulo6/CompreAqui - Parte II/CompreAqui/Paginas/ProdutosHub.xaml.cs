7860964f094f6cd50ec3ee3ed88cb7d50d2351ac
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2014-02-20 03:53:12 -0300

    Commit diário

---
+++ Capitulo6/CompreAqui - Parte II/CompreAqui/Paginas/ProdutosHub.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CompreAqui.Modelos;
+
+namespace CompreAqui.Paginas
+{
+    public partial class ProdutosHub : PhoneApplicationPage
+    {
+        public ProdutosHub()
+        {
+            InitializeComponent();
+        }
+
+        private void SuaConta_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Paginas/SuaConta.xaml", UriKind.Relative));
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Categorias.ItemsSource = (from produtos in Loja.Dados.Produtos
+                                      select new
+                                      {
+                                          Id = produtos.Categoria.Id,
+                                          Descricao = produtos.Categoria.Descricao
+                                      }).Distinct().ToList();
+
+            Promocoes.ItemsSource = Loja.Dados.Produtos.Where(produto => produto.PrecoPromocao != 0).ToList();
+        }
+    }
+}
